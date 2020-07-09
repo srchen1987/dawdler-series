e17329e2b5c1ac1e1c24083c3e4ce7eb49c58a11
jackson.song <suxuan696@gmail.com>
2020-07-09 22:25:32 +0800

    优化类加载器

--- dawdler/springboot-validator/src/main/java/com/anywide/springboot/interceptor/ValidateAdapter.java
+++ dawdler/springboot-validator/src/main/java/com/anywide/springboot/interceptor/ValidateAdapter.java
@@ -1,14 +1,16 @@
 package com.anywide.springboot.interceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
-public class ValidateAdapter extends WebMvcConfigurerAdapter {
+public class ValidateAdapter implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //添加拦截器
-        registry.addInterceptor(new ValidateInterceptorForPTTL())
-                .addPathPatterns("/**");
-        super.addInterceptors(registry);
+//        registry.addInterceptor(new ValidateInterceptorForPTTL())
+//                .addPathPatterns("/**");
+    	 registry.addInterceptor(new ValidateInterceptor())
+         .addPathPatterns("/**");
+//        super.addInterceptors(registry);
     }
 }
