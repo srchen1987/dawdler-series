6176e4f1ea40a0ef986af4b4e3f2d24f7438599b
srchen1987 <suxuan696@163.com>
2021-11-29 17:22:41 +0800

    add pinpoint plug
    Former-commit-id: a45aceda8045561ad848f8d8036e3a1e084faad2

--- README.md
+++ README.md
@@ -146,29 +146,25 @@ redis池插件,通过jedis实现.
 
 #### 20. [dawdler-es-plug](dawdler/dawdler-es-plug/README.md)
 
-通过pool2对elasticsearch-rest-high-level-client进行封装实现的一套es连接池.
+通过pool2对elasticsearch-rest-high-level-client进行封装实现的一套es连接池.  
 
-### dawdler服务器结构说明
+#### 21. [dawdler-pinpoint-plug](dawdler/dawdler-pinpoint-plug/README.md)
 
-        --bin 存放dawdler启动的jar包与脚本.
+dawdler实现pinpoint链路追踪插件.
 
-        --conf dawdler的配置信息,server-conf是服务器配置,datasources数据源配置.
+### dawdler-runtime介绍
 
-        --deploys 部署dawdler的项目,如写了一个 user模块,admin模块,那下面就是user目录,admin目录,这与tomcat的webapps很像,更像jboss/WildFly的deploy的概念.每个目录的资源是独立使用的,包括类加载器.
-
-        --lib 这里面的jar包是所有deploys下面的模块项目通用的,比如mysql驱动,数据库连接池等等相关的jar包 可以放到这里.
-
-        --logs 存放日志
+参考[dawdler-runtime](https://github.com/srchen1987/dawdler-runtime/blob/main/README.md)
 
 ### 安装教程
 
-1. 下载dawdler服务器
+1. 下载[dawdler-runtime](https://github.com/srchen1987/dawdler-runtime/archive/refs/tags/0.0.2-RELEASES.zip)
 
-2. 启动dawdler,  
-进入dawdler的bin目录,win环境`dawdler.bat`
-linux或mac环境`sh dawdler.sh`
+2. 启动zookeeper.执行 `sh zkServer.sh start`  启动zookeeper.
 
-   `sh dawdler.sh`会有以下输出
+3. 进入dawdler的bin目录,通过 `sh dawdler.sh run` 启动(win环境`dawdler.bat` linux或mac环境`sh dawdler.sh`).
+
+    运行`sh dawdler.sh`会有以下输出
 
         commands:
         run               Start dawdler in the current window 在当前窗口启动dawdler
@@ -178,7 +174,5 @@ linux或mac环境`sh dawdler.sh`
         stop              Stop dawdler 停止dawdler(关闭dawdler之前会拒绝所有的请求,同时等待处理完客户端的请求之后停止服务器)
 
         stopnow           Stop dawdler immediately 立刻停止dawdler,如果客户端有请求为处理完会收到一个强制停止的异常
-
-3. 启动zookeeper.执行 `sh zkServer.sh start`  启动zookeeper.
 
 #### 快速入门
---
+++ dawdler/dawdler-pinpoint-plug/src/main/java/com/navercorp/pinpoint/plugin/dawdler/DawdlerTraceMetadataProvider.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.navercorp.pinpoint.plugin.dawdler;
+
+import com.navercorp.pinpoint.common.trace.TraceMetadataProvider;
+import com.navercorp.pinpoint.common.trace.TraceMetadataSetupContext;
+
+/**
+ * @author jackson.song
+ * @version V1.0
+ * @Title DawdlerTraceMetadataProvider.java
+ * @Description pinpoint插件Metadata提供者,初始化配置
+ * @date 2021年4月03日
+ * @email suxuan696@gmail.com
+ */
+public class DawdlerTraceMetadataProvider implements TraceMetadataProvider {
+	@Override
+	public void setup(TraceMetadataSetupContext context) {
+		context.addServiceType(DawdlerConstants.DAWDLER_PROVIDER_SERVICE_TYPE);
+		context.addServiceType(DawdlerConstants.DAWDLER_CONSUMER_SERVICE_TYPE);
+		context.addServiceType(DawdlerConstants.DAWDLER_PROVIDER_SERVICE_NO_STATISTICS_TYPE);
+		context.addAnnotationKey(DawdlerConstants.DAWDLER_ARGS_ANNOTATION_KEY);
+		context.addAnnotationKey(DawdlerConstants.DAWDLER_RESULT_ANNOTATION_KEY);
+		context.addAnnotationKey(DawdlerConstants.DAWDLER_RPC_ANNOTATION_KEY);
+	}
+}
