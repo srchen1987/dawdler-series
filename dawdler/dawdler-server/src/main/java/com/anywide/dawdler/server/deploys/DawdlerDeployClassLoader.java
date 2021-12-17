b93a51b9c50e957a483f0d59497c44485b7213c8
srchen1987 <suxuan696@163.com>
2021-12-17 13:36:13 +0800

    兼容win平台openjdk,openjdk跨平台做的不好

--- dawdler/dawdler-server/src/main/java/com/anywide/dawdler/server/deploys/DawdlerDeployClassLoader.java
+++ dawdler/dawdler-server/src/main/java/com/anywide/dawdler/server/deploys/DawdlerDeployClassLoader.java
@@ -204,7 +204,7 @@ public class DawdlerDeployClassLoader extends DawdlerClassLoader {
 						return null;
 					}
 				}
-			}, acc);
+			});
 		} catch (java.security.PrivilegedActionException pae) {
 			throw (ClassNotFoundException) pae.getException();
 		}
