60535d4a19929fc8b80e9a06699c7a1e68f88ad9
srchen1987 <suxuan696@163.com>
2022-02-05 18:26:39 +0800

    update asm

--- dawdler/dawdler-util/src/main/java/com/anywide/dawdler/util/reflectasm/MethodAccess.java
+++ dawdler/dawdler-util/src/main/java/com/anywide/dawdler/util/reflectasm/MethodAccess.java
@@ -54,7 +54,7 @@ import org.objectweb.asm.Type;
  * @author jackson.song
  * @version V1.0
  * @Title MethodAccess.java
- * @Description asm的改进版 已升到asm支持jdk9
+ * @Description asm的改进版 已升到asm支持jdk17
  * @date 2012年5月16日
  * @email suxuan696@gmail.com
  */
