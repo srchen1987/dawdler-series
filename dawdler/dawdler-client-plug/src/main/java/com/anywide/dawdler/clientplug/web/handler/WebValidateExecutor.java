008ce1992181cff74e2c499717d61d8d39268ae3
srchen1987 <suxuan696@163.com>
2022-02-09 16:28:38 +0800

    add token to head and add log

--- dawdler/dawdler-client-plug/src/main/java/com/anywide/dawdler/clientplug/web/handler/WebValidateExecutor.java
+++ dawdler/dawdler-client-plug/src/main/java/com/anywide/dawdler/clientplug/web/handler/WebValidateExecutor.java
@@ -111,7 +111,6 @@ public class WebValidateExecutor {
 				} else {
 					params = JsonProcessUtil.jsonToBean(request.getInputStream(), HashMap.class);
 				}
-				params = viewForward.paramMaps();
 			} else {
 				params = viewForward.paramMaps();
 			}
@@ -141,5 +140,7 @@ public class WebValidateExecutor {
 		}
 		return true;
 	}
+	
+	
 
 }
