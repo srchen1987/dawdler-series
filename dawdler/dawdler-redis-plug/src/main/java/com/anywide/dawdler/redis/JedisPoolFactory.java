f2ef17a85e675d7d6e02a9f8fe0c3594f23a5098
srchen1987 <suxuan696@163.com>
2022-02-17 17:37:18 +0800

    add mysql text type

--- dawdler/dawdler-redis-plug/src/main/java/com/anywide/dawdler/redis/JedisPoolFactory.java
+++ dawdler/dawdler-redis-plug/src/main/java/com/anywide/dawdler/redis/JedisPoolFactory.java
@@ -41,7 +41,7 @@ import redis.clients.jedis.util.Pool;
  * @email suxuan696@gmail.com
  */
 public final class JedisPoolFactory {
-	public static Map<String, Pool<Jedis>> pools = new ConcurrentHashMap<>();
+	private static Map<String, Pool<Jedis>> pools = new ConcurrentHashMap<>();
 
 	private static Pool<Jedis> createJedisPool(String fileName) throws Exception {
 		Pool<Jedis> jedisPool = null;
