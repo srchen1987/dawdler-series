8b5e33f54419cb8829f357a099d3fa954cb47af0
srchen1987 <suxuan696@163.com>
2022-03-01 09:58:33 +0800

    rename package
    Former-commit-id: 013c80f7ec1177363a4ea42139f229e1e81baccc

--- dawdler/dawdler-server/src/main/java/com/anywide/dawdler/server/service/ServicesManager.java
+++ dawdler/dawdler-server/src/main/java/com/anywide/dawdler/server/service/ServicesManager.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.anywide.dawdler.server.serivce;
+package com.anywide.dawdler.server.service;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.ConcurrentHashMap;
