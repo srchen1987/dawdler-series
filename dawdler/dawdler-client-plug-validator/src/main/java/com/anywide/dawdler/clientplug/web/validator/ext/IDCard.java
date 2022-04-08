9a211cf16a2ca738747dd9db5025f64c9e9df572
srchen1987 <suxuan696@163.com>
2022-04-08 11:02:08 +0800

    支持@RequestBody与antPath校验

--- dawdler/dawdler-client-plug-validator/src/main/java/com/anywide/dawdler/clientplug/web/validator/ext/IDCard.java
+++ dawdler/dawdler-client-plug-validator/src/main/java/com/anywide/dawdler/clientplug/web/validator/ext/IDCard.java
@@ -25,6 +25,14 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * @author jackson.song
+ * @version V1.0
+ * @Title IDCard.java
+ * @Description 身份证验证
+ * @date 2007年7月21日
+ * @email suxuan696@gmail.com
+ */
 public class IDCard {
 	private static final Map<String, String> hashtable = new HashMap<>();
 
