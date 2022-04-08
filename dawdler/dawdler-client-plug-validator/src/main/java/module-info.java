78344d950a676c2af6636f5e12507e5d36ce5caf
srchen1987 <suxuan696@163.com>
2022-04-08 15:25:12 +0800

    修复合并冲突
    Former-commit-id: a99270843ee8349062719be33d3ed93197a58d87

--- dawdler/dawdler-client-plug-validator/src/main/java/module-info.java
+++ dawdler/dawdler-client-plug-validator/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module dawdler.client.plug.validator {
 	exports com.anywide.dawdler.clientplug.web.validator;
 	exports com.anywide.dawdler.clientplug.web.validator.entity;
 	exports com.anywide.dawdler.clientplug.web.validator.webbind;
+	exports com.anywide.dawdler.clientplug.web.validator.exception;
 	requires java.base;
 	requires dawdler.util;
 	requires org.slf4j;
