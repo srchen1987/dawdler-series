55eb75f46b7d77a6f772e96612a255294af5af9e
srchen1987 <suxuan696@163.com>
2022-04-17 21:29:37 +0800

    update base result

--- dawdler/dawdler-client-plug/src/main/java/com/anywide/dawdler/clientplug/web/result/PageResult.java
+++ dawdler/dawdler-client-plug/src/main/java/com/anywide/dawdler/clientplug/web/result/PageResult.java
@@ -38,7 +38,7 @@ public class PageResult<T> extends BaseResult<T> {
 	}
 
 	public PageResult(T data, Page page, boolean success) {
-		super(data, success);
+		super(data);
 		this.page = page;
 	}
 
