cdc146bd8744da665a6df7683b2b7dffc006e736
srchen1987 <suxuan696@163.com>
2022-05-03 21:04:42 +0800

    upgrade & del PlugInit

--- dawdler/dawdler-config-plug/dawdler-client-plug-config/src/main/java/module-info.java
+++ dawdler/dawdler-config-plug/dawdler-client-plug-config/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 import com.anywide.dawdler.clientplug.conf.fire.ConfigLoaderFire;
-import com.anywide.dawdler.clientplug.conf.init.ClientConfigInit;
+import com.anywide.dawdler.clientplug.conf.resource.ConfigLifeCycle;
 import com.anywide.dawdler.clientplug.load.classloader.RemoteClassLoaderFire;
 import com.anywide.dawdler.core.component.resource.ComponentLifeCycle;
 
@@ -7,7 +7,7 @@ module dawdler.client.plug.config {
 	uses RemoteClassLoaderFire;
 	provides RemoteClassLoaderFire with ConfigLoaderFire;
 	uses ComponentLifeCycle;
-	provides ComponentLifeCycle with ClientConfigInit;
+	provides ComponentLifeCycle with ConfigLifeCycle;
 	requires dawdler.config.core;
 	requires dawdler.client.plug;
 	requires dawdler.core;
--- dawdler/dawdler-config-plug/dawdler-server-plug-config/src/main/java/module-info.java
+++ dawdler/dawdler-config-plug/dawdler-server-plug-config/src/main/java/module-info.java
@@ -1,9 +1,9 @@
 import com.anywide.dawdler.core.component.resource.ComponentLifeCycle;
-import com.anywide.dawdler.serverplug.conf.init.ServerConfigInit;
+import com.anywide.dawdler.serverplug.conf.resource.ConfigLifeCycle;
 
 module dawdler.server.plug.config {
 	uses ComponentLifeCycle;
-	provides ComponentLifeCycle with ServerConfigInit;
+	provides ComponentLifeCycle with ConfigLifeCycle;
 	requires dawdler.config.core;
 	requires dawdler.core;
 	requires dawdler.server;
