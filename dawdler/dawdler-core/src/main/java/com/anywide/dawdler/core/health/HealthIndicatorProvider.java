0792431aeeccb573c05324b33d6a2a9c81045607
srchen1987 <suxuan696@163.com>
2022-05-19 14:51:41 +0800

    加入健康检测模块

---
+++ dawdler/dawdler-core/src/main/java/com/anywide/dawdler/core/health/HealthIndicatorProvider.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anywide.dawdler.core.health;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.anywide.dawdler.core.annotation.Order;
+import com.anywide.dawdler.core.order.OrderComparator;
+import com.anywide.dawdler.core.order.OrderData;
+
+/**
+ * @author jackson.song
+ * @version V1.0
+ * @Title HealthIndicatorProvider.java
+ * @Description HealthIndicatorProvider 检测提供者 通过SPI接入
+ * @date 2022年5月3日
+ * @email suxuan696@gmail.com
+ */
+public class HealthIndicatorProvider {
+	private final List<OrderData<HealthIndicator>> indicators = new ArrayList<>();
+	private static Map<String, HealthIndicatorProvider> instances = new ConcurrentHashMap<>();
+	public static HealthIndicatorProvider getInstance(String serviceName) {
+		HealthIndicatorProvider healthChecker = instances.get(serviceName);
+		if (healthChecker != null)
+			return healthChecker;
+		synchronized (instances) {
+			healthChecker = instances.get(serviceName);
+			if (healthChecker == null) {
+				instances.put(serviceName, new HealthIndicatorProvider());
+			}
+		}
+		return instances.get(serviceName);
+	}
+
+	private HealthIndicatorProvider() {
+		ServiceLoader.load(HealthIndicator.class).forEach(indicator -> {
+			OrderData<HealthIndicator> orderData = new OrderData<>();
+			Order order = indicator.getClass().getAnnotation(Order.class);
+			if (order != null) {
+				orderData.setOrder(order.value());
+			}
+			orderData.setData(indicator);
+			indicators.add(orderData);
+		});
+		OrderComparator.sort(indicators);
+	}
+
+	public List<OrderData<HealthIndicator>> getHealthIndicators() {
+		return indicators;
+	}
+	
+}
---
+++ dawdler/dawdler-redis-plug/dawdler-redis-core/src/main/java/com/anywide/dawdler/redis/health/JedisIndicator.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anywide.dawdler.redis.health;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import com.anywide.dawdler.core.health.Health;
+import com.anywide.dawdler.core.health.Health.Builder;
+import com.anywide.dawdler.core.health.HealthIndicator;
+import com.anywide.dawdler.redis.JedisPoolFactory;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.util.Pool;
+
+/**
+ *
+ * @author jackson.song
+ * @version V1.0
+ * @Title JedisIndicator.java
+ * @Description JedisIndicator jedis健康指示器
+ * @date 2022年5月1日
+ * @email suxuan696@gmail.com
+ */
+public class JedisIndicator implements HealthIndicator {
+
+	@Override
+	public String name() {
+		return "jedis";
+	}
+
+	@Override
+	public Health check(Builder builder) throws Exception {
+		Map<String, Pool<Jedis>> pools = JedisPoolFactory.getPools();
+		Set<Map.Entry<String, Pool<Jedis>>> entrySet = pools.entrySet();
+		for (Entry<String, Pool<Jedis>> entry : entrySet) {
+			String key = entry.getKey();
+			Pool<Jedis> pool = entry.getValue();
+			Jedis jedis = null;
+			Builder childBuilder = Health.up();
+			try {
+				jedis = pool.getResource();
+				String version = jedis.info("Server").split("\r\n")[1];
+				childBuilder.withDetail("version", version);
+				builder.withDetail(key, childBuilder.build().getData());
+			} catch (Exception e) {
+				throw e;
+			} finally {
+				if (jedis != null) {
+					jedis.close();
+				}
+			}
+		}
+		return builder.build();
+	}
+
+}
