16b47583969e35c3dada62e1669758bca8e6d9d0
srchen1987 <suxuan696@163.com>
2022-05-19 14:51:41 +0800

    加入健康检测模块
    Former-commit-id: ccd03cce7a3fdff023226681bc74b78c66b43be5

---
+++ dawdler/dawdler-core/src/main/java/com/anywide/dawdler/core/httpserver/Auth.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anywide.dawdler.core.httpserver;
+
+import com.sun.net.httpserver.BasicAuthenticator;
+
+/**
+ * @author jackson.song
+ * @version V1.0
+ * @Title Auth.java
+ * @Description Auth 认证器
+ * @date 2022年5月1日
+ * @email suxuan696@gmail.com
+ */
+public class Auth extends BasicAuthenticator {
+	private String username;
+	private String password;
+
+	public Auth(String realm, String username, String password) {
+		super(realm);
+		this.username = username;
+		this.password = password;
+	}
+
+	@Override
+	public boolean checkCredentials(String username, String password) {
+		if (this.username.equals(username) && this.password.equals(password))
+			return true;
+		return false;
+	}
+
+}
