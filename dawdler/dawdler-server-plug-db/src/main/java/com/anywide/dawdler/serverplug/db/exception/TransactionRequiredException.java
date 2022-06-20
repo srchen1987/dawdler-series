658f20de39acf1430d1138a07c9201db119646a5
srchen1987 <suxuan696@163.com>
2022-06-20 09:49:02 +0800

    统一配置中心支持db配置
    Former-commit-id: d2fb2a0b5de428d1a7ea83ba661d764544ecc284

--- dawdler/dawdler-server-plug-db/src/main/java/com/anywide/dawdler/serverplug/db/exception/TransactionRequiredException.java
+++ dawdler/dawdler-server-plug-db/src/main/java/com/anywide/dawdler/serverplug/db/exception/TransactionRequiredException.java
@@ -1,15 +1,29 @@
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.anywide.dawdler.serverplug.db.exception;
 
 /**
-*
-* @Title TransactionRequiredException.java
-* @Description 代替javax.transaction.TransactionRequiredException，jdk8之后移除了TransactionRequiredException注解
-* @author jackson.song
-* @date 2021年12月18日
-* @version V1.0
-* @email suxuan696@gmail.com
-*/
+ * @author jackson.song
+ * @version V1.0
+ * @Title TransactionRequiredException.java
+ * @Description 代替javax.transaction.TransactionRequiredException
+ * @date 2021年12月18日
+ * @email suxuan696@gmail.com
+ */
 public class TransactionRequiredException extends RuntimeException{
 
 	private static final long serialVersionUID = -883131451486676208L;
