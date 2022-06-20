0859aaeb1d6c898acd402148d786a192865a59a1
srchen1987 <suxuan696@163.com>
2022-06-20 09:34:39 +0800

    update log & add comment

--- dawdler/dawdler-core/src/main/java/com/anywide/dawdler/core/component/resource/ComponentLifeCycleProvider.java
+++ dawdler/dawdler-core/src/main/java/com/anywide/dawdler/core/component/resource/ComponentLifeCycleProvider.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.anywide.dawdler.core.component.resource;
 
 import java.util.ArrayList;
@@ -10,6 +26,14 @@ import com.anywide.dawdler.core.annotation.Order;
 import com.anywide.dawdler.core.order.OrderComparator;
 import com.anywide.dawdler.core.order.OrderData;
 
+/**
+ * @author jackson.song
+ * @version V1.0
+ * @Title ComponentLifeCycleProvider.java
+ * @Description 生命周期接口提供者
+ * @date 2022年4月12日
+ * @email suxuan696@gmail.com
+ */
 public class ComponentLifeCycleProvider {
 	private static Map<String, ComponentLifeCycleProvider> instances = new ConcurrentHashMap<>();
 	private final List<OrderData<ComponentLifeCycle>> componentLifeCycles = new ArrayList<>();
