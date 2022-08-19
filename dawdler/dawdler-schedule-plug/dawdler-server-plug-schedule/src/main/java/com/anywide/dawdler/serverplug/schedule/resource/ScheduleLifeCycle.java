cfc01ebc272c1dfc3cf130c1159fe13bb94cfb9b
srchen1987 <suxuan696@163.com>
2022-08-19 11:14:30 +0800

    update doc & add schedule

---
+++ dawdler/dawdler-schedule-plug/dawdler-server-plug-schedule/src/main/java/com/anywide/dawdler/serverplug/schedule/resource/ScheduleLifeCycle.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anywide.dawdler.serverplug.schedule.resource;
+
+import java.util.List;
+
+import com.anywide.dawdler.core.component.resource.ComponentLifeCycle;
+import com.anywide.dawdler.core.order.OrderData;
+import com.anywide.dawdler.schedule.ScheduleInit;
+import com.anywide.dawdler.server.context.DawdlerContext;
+import com.anywide.dawdler.server.deploys.ServiceBase;
+import com.anywide.dawdler.server.filter.DawdlerFilter;
+import com.anywide.dawdler.server.filter.FilterProvider;
+import com.anywide.dawdler.server.listener.DawdlerListenerProvider;
+import com.anywide.dawdler.server.listener.DawdlerServiceListener;
+
+/**
+ * @author jackson.song
+ * @version V1.0
+ * @Title ScheduleLifeCycle.java
+ * @Description 实现向监听器,过滤器 schedule注解方法的调用
+ * @date 2022年7月12日
+ * @email suxuan696@gmail.com
+ */
+public class ScheduleLifeCycle implements ComponentLifeCycle {
+	@Override
+	public void init() throws Exception {
+		DawdlerContext dawdlerContext = DawdlerContext.getDawdlerContext();
+		FilterProvider filterProvider = (FilterProvider) dawdlerContext.getAttribute(ServiceBase.FILTER_PROVIDER);
+		DawdlerListenerProvider dawdlerListenerProvider = (DawdlerListenerProvider) dawdlerContext
+				.getAttribute(ServiceBase.DAWDLER_LISTENER_PROVIDER);
+		init(dawdlerListenerProvider.getListeners(), filterProvider.getFilters());
+	}
+
+	public void init(List<OrderData<DawdlerServiceListener>> dawdlerServiceListeners,
+			List<OrderData<DawdlerFilter>> dawdlerFilters) throws Exception {
+		for (OrderData<DawdlerServiceListener> orderData : dawdlerServiceListeners) {
+			DawdlerServiceListener dawdlerServiceListener = orderData.getData();
+			ScheduleInit.initScheduler(dawdlerServiceListener, dawdlerServiceListener.getClass());
+		}
+		for (OrderData<DawdlerFilter> orderData : dawdlerFilters) {
+			DawdlerFilter dawdlerFilter = orderData.getData();
+			ScheduleInit.initScheduler(dawdlerFilter, dawdlerFilter.getClass());
+		}
+	}
+
+}
