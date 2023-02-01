62c50b726b7b37634f14900029a58f937a2a06ab
srchen1987 <suxuan696@163.com>
2023-02-01 18:32:38 +0800

    add doc & No longer rely on zookeeper EPHEMERAL feature

--- dawdler/dawdler-util/src/main/java/module-info.java
+++ dawdler/dawdler-util/src/main/java/module-info.java
@@ -1,12 +1,12 @@
 module dawdler.util {
 	requires java.base;
 	requires java.management;
-	requires transitive com.fasterxml.jackson.core;
-	requires transitive com.fasterxml.jackson.databind;
+	requires com.fasterxml.jackson.core;
+	requires com.fasterxml.jackson.databind;
 	requires org.objectweb.asm;
 	requires org.objectweb.asm.tree;
 	requires transitive java.xml;
-	requires transitive dom4j;
+	requires dom4j;
 	requires org.slf4j;
 
 	exports com.anywide.dawdler.util;
