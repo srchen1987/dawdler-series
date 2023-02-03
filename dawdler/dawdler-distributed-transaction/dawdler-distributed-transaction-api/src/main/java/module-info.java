cb0e13b0e1949056b22b1c0ac154ca86c7f64998
srchen1987 <suxuan696@163.com>
2023-02-03 14:55:57 +0800

    update pom & remove api
    Former-commit-id: 389c0693c7096f90e89e577af7dacf74edb157b4

--- dawdler/dawdler-distributed-transaction/dawdler-distributed-transaction-api/src/main/java/module-info.java
+++ dawdler/dawdler-distributed-transaction/dawdler-distributed-transaction-api/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module dawdler.distributed.transaction.api {
 	requires java.base;
+	requires org.aspectj.runtime;
 
+	exports com.anywide.dawdler.distributed.transaction.interceptor;
 	exports com.anywide.dawdler.distributed.transaction.annotation;
 	exports com.anywide.dawdler.distributed.transaction.context;
 	exports com.anywide.dawdler.distributed.transaction;
