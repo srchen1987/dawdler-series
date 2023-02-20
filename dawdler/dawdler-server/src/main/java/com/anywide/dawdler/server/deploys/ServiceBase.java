6084bb3227833a29710778a2f56507afee13e856
srchen1987 <suxuan696@163.com>
2023-02-20 09:38:35 +0800

    add resetContextClassLoader
    Former-commit-id: d01fd3865d5bbd2c1a160702f0152639d0ca10f6

--- dawdler/dawdler-server/src/main/java/com/anywide/dawdler/server/deploys/ServiceBase.java
+++ dawdler/dawdler-server/src/main/java/com/anywide/dawdler/server/deploys/ServiceBase.java
@@ -110,7 +110,7 @@ public class ServiceBase implements Service {
 		this.deployName = deploy.getName();
 		this.status = Status.STARTING;
 		classLoader = DawdlerDeployClassLoader.createLoader(binPath, parent, getClassLoaderURL());
-		Thread.currentThread().setContextClassLoader(classLoader);
+		resetContextClassLoader();
 		dawdlerContext = new DawdlerContext(classLoader, deploy.getName(), deploy.getPath(), getClassesDir().getPath(),
 				host, port, servicesManager, antPathMatcher);
 		classLoader.setDawdlerContext(dawdlerContext);
@@ -151,6 +151,7 @@ public class ServiceBase implements Service {
 
 	@Override
 	public void start() throws Throwable {
+		resetContextClassLoader();
 		List<OrderData<ComponentLifeCycle>> lifeCycleList = ComponentLifeCycleProvider.getInstance(deployName)
 				.getComponentLifeCycles();
 		for (int i = 0; i < lifeCycleList.size(); i++) {
@@ -275,6 +276,7 @@ public class ServiceBase implements Service {
 
 	@Override
 	public void prepareStop() {
+		resetContextClassLoader();
 		List<OrderData<ComponentLifeCycle>> lifeCycleList = ComponentLifeCycleProvider.getInstance(deployName)
 				.getComponentLifeCycles();
 		for (int i = lifeCycleList.size() - 1; i >= 0; i--) {
@@ -289,6 +291,7 @@ public class ServiceBase implements Service {
 
 	@Override
 	public void stop() {
+		resetContextClassLoader();
 		if (dawdlerListenerProvider.getListeners() != null) {
 			for (int i = dawdlerListenerProvider.getListeners().size(); i > 0; i--) {
 				try {
@@ -386,6 +389,10 @@ public class ServiceBase implements Service {
 		}
 		return null;
 	}
+	
+	private void resetContextClassLoader() {
+		Thread.currentThread().setContextClassLoader(classLoader);
+	}
 
 	public class DeployScanner {
 		private Set<String> packagePathInJar = new LinkedHashSet<String>();
@@ -456,7 +463,7 @@ public class ServiceBase implements Service {
 			serviceHealth.addComponent("error", cause.getClass().getName() + ":" + cause.getMessage());
 			return serviceHealth;
 		}
-		Thread.currentThread().setContextClassLoader(classLoader);
+		resetContextClassLoader();
 		ServiceHealth serviceHealth = new ServiceHealth(deployName);
 		serviceHealth.setStatus(Status.STARTING);
 		HealthIndicatorProvider healthChecker = HealthIndicatorProvider.getInstance(deployName);
