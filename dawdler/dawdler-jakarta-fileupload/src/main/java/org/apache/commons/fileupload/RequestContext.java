396c7798822cb3eac9448c76a3155a753ec1d4d8
srchen1987 <suxuan696@163.com>
2023-02-28 16:22:32 +0800

    upgrade fileupload to 1.5

--- dawdler/dawdler-jakarta-fileupload/src/main/java/org/apache/commons/fileupload/RequestContext.java
+++ dawdler/dawdler-jakarta-fileupload/src/main/java/org/apache/commons/fileupload/RequestContext.java
@@ -16,50 +16,48 @@
  */
 package org.apache.commons.fileupload;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.io.IOException;
 
 /**
- * <p>
- * Abstracts access to the request information needed for file uploads. This
- * interface should be implemented for each type of request that may be handled
- * by FileUpload, such as servlets and portlets.
- * </p>
+ * <p>Abstracts access to the request information needed for file uploads. This
+ * interface should be implemented for each type of request that may be
+ * handled by FileUpload, such as servlets and portlets.</p>
  *
  * @since FileUpload 1.1
  */
 public interface RequestContext {
 
-	/**
-	 * Retrieve the character encoding for the request.
-	 *
-	 * @return The character encoding for the request.
-	 */
-	String getCharacterEncoding();
+    /**
+     * Retrieve the character encoding for the request.
+     *
+     * @return The character encoding for the request.
+     */
+    String getCharacterEncoding();
 
-	/**
-	 * Retrieve the content type of the request.
-	 *
-	 * @return The content type of the request.
-	 */
-	String getContentType();
+    /**
+     * Retrieve the content type of the request.
+     *
+     * @return The content type of the request.
+     */
+    String getContentType();
 
-	/**
-	 * Retrieve the content length of the request.
-	 *
-	 * @return The content length of the request.
-	 * @deprecated 1.3 Use {@link UploadContext#contentLength()} instead
-	 */
-	@Deprecated
-	int getContentLength();
+    /**
+     * Retrieve the content length of the request.
+     *
+     * @return The content length of the request.
+     * @deprecated 1.3 Use {@link UploadContext#contentLength()} instead
+     */
+    @Deprecated
+    int getContentLength();
 
-	/**
-	 * Retrieve the input stream for the request.
-	 *
-	 * @return The input stream for the request.
-	 *
-	 * @throws IOException if a problem occurs.
-	 */
-	InputStream getInputStream() throws IOException;
+    /**
+     * Retrieve the input stream for the request.
+     *
+     * @return The input stream for the request.
+     *
+     * @throws IOException if a problem occurs.
+     */
+    InputStream getInputStream() throws IOException;
 
 }
--- dawdler/dawdler-jakarta-fileupload/src/main/java/org/apache/commons/fileupload/servlet/ServletRequestContext.java
+++ dawdler/dawdler-jakarta-fileupload/src/main/java/org/apache/commons/fileupload/servlet/ServletRequestContext.java
@@ -21,110 +21,110 @@ import static java.lang.String.format;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.fileupload.FileUploadBase;
 import org.apache.commons.fileupload.UploadContext;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 /**
- * <p>
- * Provides access to the request information needed for a request made to an
- * HTTP servlet.
- * </p>
+ * <p>Provides access to the request information needed for a request made to
+ * an HTTP servlet.</p>
  *
  * @since FileUpload 1.1
  */
 public class ServletRequestContext implements UploadContext {
 
-	// ----------------------------------------------------- Instance Variables
+    // ----------------------------------------------------- Instance Variables
 
-	/**
-	 * The request for which the context is being provided.
-	 */
-	private final HttpServletRequest request;
+    /**
+     * The request for which the context is being provided.
+     */
+    private final HttpServletRequest request;
 
-	// ----------------------------------------------------------- Constructors
+    // ----------------------------------------------------------- Constructors
 
-	/**
-	 * Construct a context for this request.
-	 *
-	 * @param request The request to which this context applies.
-	 */
-	public ServletRequestContext(HttpServletRequest request) {
-		this.request = request;
-	}
+    /**
+     * Construct a context for this request.
+     *
+     * @param request The request to which this context applies.
+     */
+    public ServletRequestContext(HttpServletRequest request) {
+        this.request = request;
+    }
 
-	// --------------------------------------------------------- Public Methods
+    // --------------------------------------------------------- Public Methods
 
-	/**
-	 * Retrieve the character encoding for the request.
-	 *
-	 * @return The character encoding for the request.
-	 */
-	@Override
-	public String getCharacterEncoding() {
-		return request.getCharacterEncoding();
-	}
+    /**
+     * Retrieve the character encoding for the request.
+     *
+     * @return The character encoding for the request.
+     */
+    @Override
+    public String getCharacterEncoding() {
+        return request.getCharacterEncoding();
+    }
 
-	/**
-	 * Retrieve the content type of the request.
-	 *
-	 * @return The content type of the request.
-	 */
-	@Override
-	public String getContentType() {
-		return request.getContentType();
-	}
+    /**
+     * Retrieve the content type of the request.
+     *
+     * @return The content type of the request.
+     */
+    @Override
+    public String getContentType() {
+        return request.getContentType();
+    }
 
-	/**
-	 * Retrieve the content length of the request.
-	 *
-	 * @return The content length of the request.
-	 * @deprecated 1.3 Use {@link #contentLength()} instead
-	 */
-	@Override
-	@Deprecated
-	public int getContentLength() {
-		return request.getContentLength();
-	}
+    /**
+     * Retrieve the content length of the request.
+     *
+     * @return The content length of the request.
+     * @deprecated 1.3 Use {@link #contentLength()} instead
+     */
+    @Override
+    @Deprecated
+    public int getContentLength() {
+        return request.getContentLength();
+    }
 
-	/**
-	 * Retrieve the content length of the request.
-	 *
-	 * @return The content length of the request.
-	 * @since 1.3
-	 */
-	@Override
-	public long contentLength() {
-		long size;
-		try {
-			size = Long.parseLong(request.getHeader(FileUploadBase.CONTENT_LENGTH));
-		} catch (NumberFormatException e) {
-			size = request.getContentLength();
-		}
-		return size;
-	}
+    /**
+     * Retrieve the content length of the request.
+     *
+     * @return The content length of the request.
+     * @since 1.3
+     */
+    @Override
+    public long contentLength() {
+        long size;
+        try {
+            size = Long.parseLong(request.getHeader(FileUploadBase.CONTENT_LENGTH));
+        } catch (NumberFormatException e) {
+            size = request.getContentLength();
+        }
+        return size;
+    }
 
-	/**
-	 * Retrieve the input stream for the request.
-	 *
-	 * @return The input stream for the request.
-	 *
-	 * @throws IOException if a problem occurs.
-	 */
-	@Override
-	public InputStream getInputStream() throws IOException {
-		return request.getInputStream();
-	}
+    /**
+     * Retrieve the input stream for the request.
+     *
+     * @return The input stream for the request.
+     *
+     * @throws IOException if a problem occurs.
+     */
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return request.getInputStream();
+    }
 
-	/**
-	 * Returns a string representation of this object.
-	 *
-	 * @return a string representation of this object.
-	 */
-	@Override
-	public String toString() {
-		return format("ContentLength=%s, ContentType=%s", Long.valueOf(this.contentLength()), this.getContentType());
-	}
+    /**
+     * Returns a string representation of this object.
+     *
+     * @return a string representation of this object.
+     */
+    @Override
+    public String toString() {
+        return format("ContentLength=%s, ContentType=%s",
+                Long.valueOf(this.contentLength()),
+                this.getContentType());
+    }
 
 }
