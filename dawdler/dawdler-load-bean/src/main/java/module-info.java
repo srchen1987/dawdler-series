fe8b23c3570235bec03bd299d662c517d885f85d
srchen1987 <suxuan696@163.com>
2023-05-04 23:42:26 +0800

    update doc & move BaseResult PageResult to load-web

--- dawdler/dawdler-load-bean/src/main/java/module-info.java
+++ dawdler/dawdler-load-bean/src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module dawdler.load.bean {
 	requires java.base;
 	requires transitive org.dom4j;
+	requires com.fasterxml.jackson.core;
+	requires com.fasterxml.jackson.databind;
 
 	exports com.anywide.dawdler.serverplug.bean;
 	exports com.anywide.dawdler.serverplug.load.bean;
+	exports com.anywide.dawdler.core.result;
 }
