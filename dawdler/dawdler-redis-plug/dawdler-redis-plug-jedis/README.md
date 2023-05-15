d42c252c41e94a1322257008d5e2dc59c5c817cd
srchen1987 <suxuan696@163.com>
2023-05-15 11:03:21 +0800

    rename redis module to jedis

---
+++ dawdler/dawdler-redis-plug/dawdler-redis-plug-jedis/README.md
@@ -0,0 +1,13 @@
+# dawdler-redis-plug-jedis
+
+## 模块介绍
+
+jedis实现的redis模块.
+
+### 1. 子模块介绍
+
+[dawdler-jedis-core jedis实现redis的插件,支持单机模式与哨兵模式.](./dawdler-jedis-core/README.md)
+
+[dawdler-server-plug-jedis 实现dawdler-server端注入功能.](./dawdler-server-plug-jedis/README.md)
+
+[dawdler-client-plug-jedis 实现dawdler-client端注入功能.](./dawdler-client-plug-jedis/README.md)
---
+++ dawdler/dawdler-redis-plug/dawdler-redis-plug-jedis/dawdler-jedis-core/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+import com.anywide.dawdler.core.component.resource.ComponentLifeCycle;
+import com.anywide.dawdler.core.health.HealthIndicator;
+import com.anywide.dawdler.jedis.health.JedisIndicator;
+import com.anywide.dawdler.jedis.resource.JedisLifeCycle;
+
+module dawdler.jedis.core {
+	requires java.base;
+	requires dawdler.core;
+	requires dawdler.util;
+	requires org.slf4j;
+	requires org.apache.commons.pool2;
+	requires redis.clients.jedis;
+
+	exports com.anywide.dawdler.jedis;
+
+	uses ComponentLifeCycle;
+
+	provides ComponentLifeCycle with JedisLifeCycle;
+
+	uses HealthIndicator;
+
+	provides HealthIndicator with JedisIndicator;
+}
