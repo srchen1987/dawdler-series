6cc8c4a34c155a8df33b7fb6bb3c0206100797b2
srchen1987 <suxuan696@163.com>
2023-05-15 11:03:21 +0800

    rename redis module to jedis
    Former-commit-id: 57d5377acd11822b6a09b5aed16fabe48c5e075b

---
+++ dawdler/dawdler-redis-plug/dawdler-redis-plug-jedis/dawdler-server-plug-jedis/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+import com.anywide.dawdler.core.component.resource.ComponentLifeCycle;
+import com.anywide.dawdler.serverplug.jedis.resource.JedisLifeCycle;
+
+module dawdler.server.plug.jedis {
+	requires java.base;
+	requires dawdler.core;
+	requires dawdler.server.plug;
+	requires dawdler.server;
+	requires dawdler.jedis.core;
+
+	uses ComponentLifeCycle;
+
+	provides ComponentLifeCycle with JedisLifeCycle;
+}
