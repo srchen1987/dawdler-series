3f6354c8996e8a84072c08f69bdfc426adc26566
srchen1987 <suxuan696@163.com>
2023-11-08 16:28:26 +0800

    upgrade mybatis

---
+++ dawdler/dawdler-server-plug-mybatis/src/main/java/com/anywide/dawdler/serverplug/db/mybatis/executor/SimpleExecutor.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anywide.dawdler.serverplug.db.mybatis.executor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.ibatis.cursor.Cursor;
+import org.apache.ibatis.executor.BatchResult;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.transaction.Transaction;
+
+/**
+ * @author jackson.song
+ * @version V1.0
+ * @Title SimpleExecutor.java
+ * @Description 执行器继承NoCacheBaseExecutor
+ * 因为优化将session改为single模式 所以不支持其他的BatchExecutor, ReuseExecutor 有batch的需求可以采用jdbc batch方式处理 效率更高
+ * @date 2023年11月7日
+ * @email suxuan696@gmail.com
+ */
+public class SimpleExecutor extends NoCacheBaseExecutor {
+
+	public SimpleExecutor(Configuration configuration, Transaction transaction) {
+		super(configuration, transaction);
+	}
+
+	@Override
+	public int doUpdate(MappedStatement ms, Object parameter) throws SQLException {
+		Statement stmt = null;
+		try {
+			Configuration configuration = ms.getConfiguration();
+			StatementHandler handler = configuration.newStatementHandler(this, ms, parameter, RowBounds.DEFAULT, null,
+					null);
+			stmt = prepareStatement(handler, ms.getStatementLog());
+			return handler.update(stmt);
+		} finally {
+			closeStatement(stmt);
+		}
+	}
+
+	@Override
+	public <E> List<E> doQuery(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler,
+			BoundSql boundSql) throws SQLException {
+		Statement stmt = null;
+		try {
+			Configuration configuration = ms.getConfiguration();
+			StatementHandler handler = configuration.newStatementHandler(this, ms, parameter, rowBounds, resultHandler,
+					boundSql);
+			stmt = prepareStatement(handler, ms.getStatementLog());
+			return handler.query(stmt, resultHandler);
+		} finally {
+			closeStatement(stmt);
+		}
+	}
+
+	@Override
+	protected <E> Cursor<E> doQueryCursor(MappedStatement ms, Object parameter, RowBounds rowBounds, BoundSql boundSql)
+			throws SQLException {
+		Configuration configuration = ms.getConfiguration();
+		StatementHandler handler = configuration.newStatementHandler(this, ms, parameter, rowBounds, null, boundSql);
+		Statement stmt = prepareStatement(handler, ms.getStatementLog());
+		Cursor<E> cursor = handler.queryCursor(stmt);
+		stmt.closeOnCompletion();
+		return cursor;
+	}
+
+	@Override
+	public List<BatchResult> doFlushStatements(boolean isRollback) {
+		return Collections.emptyList();
+	}
+
+	private Statement prepareStatement(StatementHandler handler, Log statementLog) throws SQLException {
+		Statement stmt;
+		Connection connection = getConnection(statementLog);
+		stmt = handler.prepare(connection, transaction.getTimeout());
+		handler.parameterize(stmt);
+		return stmt;
+	}
+
+}
