a112ab3405497b4fe37dc94dfa842fa6b5adfaca
srchen1987 <suxuan696@163.com>
2024-06-18 13:40:03 +0800

    upgrade

--- dawdler/dawdler-client/src/main/java/com/anywide/dawdler/client/ConnectionPool.java
+++ dawdler/dawdler-client/src/main/java/com/anywide/dawdler/client/ConnectionPool.java
@@ -22,8 +22,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
