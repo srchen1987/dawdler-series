66aac5e1c947407c727ef81309bed421be5aee8e
srchen1987 <suxuan696@163.com>
2024-10-10 23:37:15 +0800

    upgrade
    Former-commit-id: 24c55e52d77b6ae73985184bf6b5199100c44c26

--- dawdler/dawdler-client-plug-session/src/main/java/com/anywide/dawdler/clientplug/web/session/DawdlerSessionFilter.java
+++ dawdler/dawdler-client-plug-session/src/main/java/com/anywide/dawdler/clientplug/web/session/DawdlerSessionFilter.java
@@ -160,7 +160,7 @@ public class DawdlerSessionFilter implements Filter {
 		abstractDistributedSessionManager = new DistributedCaffeineSessionManager(maxInactiveInterval, maxSize, defense,
 				ipMaxInactiveInterval, ipMaxSize);
 		Object listener = servletContext
-				.getAttribute(AbstractDistributedSessionManager.DISTRIBUTED_SESSION_HTTPSESSION_LISTENER);
+				.getAttribute(AbstractDistributedSessionManager.DISTRIBUTED_SESSION_HTTP_SESSION_LISTENER);
 		if (listener instanceof HttpSessionListener) {
 			abstractDistributedSessionManager.setHttpSessionListener((HttpSessionListener)listener);
 		}
