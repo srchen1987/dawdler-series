a525bb6d3ba2148679a2f9a15f4f7db5d5a4cdda
srchen1987 <suxuan696@163.com>
2024-11-01 16:37:11 +0800

    upgrade & add web health check
    Former-commit-id: 4d3e688d3de5670f60a554859bcd17f81c8fcfec

--- dawdler/dawdler-server/src/main/java/com/anywide/dawdler/server/deploys/AbstractServiceRoot.java
+++ dawdler/dawdler-server/src/main/java/com/anywide/dawdler/server/deploys/AbstractServiceRoot.java
@@ -124,7 +124,7 @@ public abstract class AbstractServiceRoot {
 			}
 
 		};
-		httpServer.addPath("/status", handler);
+		httpServer.addPath(healthCheck.getUri() == null ? "/health" : healthCheck.getUri(), handler);
 		httpServer.start();
 	}
 
