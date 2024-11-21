5ccfd3112993714915f2d2662df2d5fbee0de953
srchen1987 <suxuan696@163.com>
2024-11-21 21:56:15 +0800

    add es https support
    Former-commit-id: 606b76ec1cca3aaf7527a0c00f97c6b49259f750

--- dawdler/dawdler-pinpoint-plug/src/main/java/com/navercorp/pinpoint/plugin/dawdler/DawdlerPlugin.java
+++ dawdler/dawdler-pinpoint-plug/src/main/java/com/navercorp/pinpoint/plugin/dawdler/DawdlerPlugin.java
@@ -25,8 +25,8 @@ import com.navercorp.pinpoint.bootstrap.instrument.Instrumentor;
 import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformCallback;
 import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformTemplate;
 import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformTemplateAware;
-import com.navercorp.pinpoint.bootstrap.logging.PLogger;
-import com.navercorp.pinpoint.bootstrap.logging.PLoggerFactory;
+import com.navercorp.pinpoint.bootstrap.logging.PluginLogManager;
+import com.navercorp.pinpoint.bootstrap.logging.PluginLogger;
 import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPlugin;
 import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPluginSetupContext;
 import com.navercorp.pinpoint.common.trace.ServiceType;
@@ -40,7 +40,7 @@ import com.navercorp.pinpoint.plugin.dawdler.interceptor.DawdlerServerIntercepto
  */
 public class DawdlerPlugin implements ProfilerPlugin, TransformTemplateAware {
 
-	private final PLogger logger = PLoggerFactory.getLogger(this.getClass());
+	private final PluginLogger logger = PluginLogManager.getLogger(getClass());
 
 	private TransformTemplate transformTemplate;
 
