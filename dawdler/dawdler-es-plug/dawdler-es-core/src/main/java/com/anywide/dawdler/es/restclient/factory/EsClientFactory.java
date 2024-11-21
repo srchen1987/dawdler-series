e97da4bc97495ea2c802d3d40ca3e2dd3bdeff7a
srchen1987 <suxuan696@163.com>
2024-11-21 21:59:19 +0800

    add es https support
    Former-commit-id: 403767983a75299cd3ff688ee73d3b71329ebb9f

--- dawdler/dawdler-es-plug/dawdler-es-core/src/main/java/com/anywide/dawdler/es/restclient/factory/EsClientFactory.java
+++ dawdler/dawdler-es-plug/dawdler-es-core/src/main/java/com/anywide/dawdler/es/restclient/factory/EsClientFactory.java
@@ -16,14 +16,27 @@
  */
 package com.anywide.dawdler.es.restclient.factory;
 
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.ssl.SSLContexts;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
+
+import com.anywide.dawdler.util.DawdlerTool;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
@@ -39,24 +52,44 @@ public class EsClientFactory {
 	private String username;
 	private String password;
 	private String hosts;
-	private int connectionRequestTimeout;// connetcion pool中获得一个connection的超时时间
+	private int connectionRequestTimeout;// connection pool中获得一个connection的超时时间
 	private int connectTimeout;// 链接建立的超时时间
 	private int socketTimeout;// 响应超时时间
+	private String keystorePath;// 证书路径
+	private String keystorePassword;// 证书密码
 
 	public EsClientFactory(String username, String password, String hosts, int connectionRequestTimeout,
-			int connectTimeout, int socketTimeout) {
+			int connectTimeout, int socketTimeout, String keystorePath, String keystorePassword) {
 		this.username = username;
 		this.password = password;
 		this.hosts = hosts;
 		this.connectionRequestTimeout = connectionRequestTimeout;
 		this.connectTimeout = connectTimeout;
 		this.socketTimeout = socketTimeout;
+		this.keystorePath = keystorePath;
+		this.keystorePassword = keystorePassword;
 	}
 
-	public ElasticsearchClient create() {
-		HttpHost[] httpHostArray = getHttpHosts();
+	public ElasticsearchClient create() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException,
+			CertificateException, IOException {
 		CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-		credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+		final SSLContext sslContext;
+		boolean isSSL = false;
+		if (keystorePassword != null && keystorePassword.length() > 0 && keystorePath != null
+				&& keystorePath.length() > 0) {
+			sslContext = SSLContexts.custom()
+					.loadTrustMaterial(DawdlerTool.getResourceURLFromClassPath(keystorePath),
+							keystorePassword.toCharArray(),
+							new TrustSelfSignedStrategy())
+					.build();
+			isSSL = true;
+		} else {
+			sslContext = null;
+		}
+		HttpHost[] httpHostArray = getHttpHosts(isSSL);
+		if (username != null && password != null && username.length() > 0 && password.length() > 0) {
+			credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+		}
 		RestClient restClient = RestClient.builder(httpHostArray)
 				.setRequestConfigCallback(new RestClientBuilder.RequestConfigCallback() {
 					@Override
@@ -66,18 +99,20 @@ public class EsClientFactory {
 					}
 				})
 				.setHttpClientConfigCallback(
-						httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
+						httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+								.setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE).setSSLContext(sslContext))
 				.build();
 		ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
 		return new ElasticsearchClient(transport);
 	}
 
-	private HttpHost[] getHttpHosts() {
+	private HttpHost[] getHttpHosts(boolean isSSL) {
 		String[] split = hosts.split(",");
 		HttpHost[] httpHostArray = new HttpHost[split.length];
 		for (int i = 0; i < split.length; i++) {
 			String item = split[i];
-			httpHostArray[i] = new HttpHost(item.split(":")[0], Integer.parseInt(item.split(":")[1]), "http");
+			httpHostArray[i] = new HttpHost(item.split(":")[0], Integer.parseInt(item.split(":")[1]),
+					isSSL ? "https" : "http");
 		}
 		return httpHostArray;
 	}
