4f1d43e7f3d4bf59a97713409b2e3d5dbe75accf
srchen1987 <suxuan696@163.com>
2024-11-28 18:19:48 +0800

    update velocity
    Former-commit-id: e4410b1052cdc84cdb86cbfa75a6f13fc7549d92

--- dawdler/dawdler-client-plug-velocity/src/main/java/com/anywide/dawdler/clientplug/velocity/template/manager/TemplateManager.java
+++ dawdler/dawdler-client-plug-velocity/src/main/java/com/anywide/dawdler/clientplug/velocity/template/manager/TemplateManager.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.anywide.dawdler.clientplug.web.view.templatemanager;
+package com.anywide.dawdler.clientplug.velocity.template.manager;
 
 import java.util.Properties;
 
